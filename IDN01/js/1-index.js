1c6026be43ddd6c0beb8d442dced704ea70e2344
叶子 谷 <1307780544@qq.com>
2024-12-16 02:10:36 +0800

    Add IDN01 folder for the second site

---
+++ IDN01/js/1-index.js
@@ -0,0 +1,840 @@
+@import url("2-css.css");
+
+*{
+   margin: 0;
+   padding: 0;
+   box - sizing: border - box;
+}
+
+.parent {
+   width: 681 px;
+   height: 384 px;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   margin: auto auto;
+   overflow: hidden;
+   position: absolute;
+   border - radius: 16 px; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+svg {
+   position: absolute;
+   z - index: 1;
+   width: 681 px;
+   height: 384 px;
+}
+
+button {
+   position: absolute;
+   z - index: 50;
+   width: 40 px;
+   overflow: hidden;
+   height: 40 px;
+   border: none;
+   border - radius: 50 % ;
+   background: #fff;
+   cursor: pointer; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+button: focus {
+   outline - width: 0;
+}
+
+circle {
+   stroke: #fff;
+   fill: none;
+   transition: 0.3 s;
+}
+
+#svg1 circle {
+   transition - timing - function: linear;
+}
+
+#svg2 circle {
+   transition - timing - function: linear;
+}
+
+#Capa_1 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-7 px, -8 px);
+}
+
+#Capa_2 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-9 px, -8 px);
+}
+
+.right {
+   margin - left: 628 px;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.right:hover {
+
+	background-color: # fff;
+}
+
+.left {
+   margin - left: 0.5 % ;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.left:hover {
+
+	background-color: # fff;
+}
+
+.circle1 {
+   transition - delay: 0.05 s;
+}
+
+.circle2 {
+   transition - delay: 0.1 s;
+}
+
+.circle3 {
+   transition - delay: 0.15 s;
+}
+
+.circle4 {
+   transition - delay: 0.2 s;
+}
+
+.circle5 {
+   transition - delay: 0.25 s;
+}
+
+.circle6 {
+   transition - delay: 0.3 s;
+}
+
+.circle7 {
+   transition - delay: 0.35 s;
+}
+
+.circle8 {
+   transition - delay: 0.4 s;
+}
+
+.circle9 {
+   transition - delay: 0.45 s;
+}
+
+.circle10 {
+   transition - delay: 0.05 s;
+}
+
+.circle11 {
+   transition - delay: 0.1 s;
+}
+
+.circle12 {
+   transition - delay: 0.15 s;
+}
+
+.circle13 {
+   transition - delay: 0.2 s;
+}
+
+.circle14 {
+   transition - delay: 0.25 s;
+}
+
+.circle15 {
+   transition - delay: 0.3 s;
+}
+
+.circle16 {
+   transition - delay: 0.35 s;
+}
+
+.circle17 {
+   transition - delay: 0.4 s;
+}
+
+.circle18 {
+   transition - delay: 0.45 s;
+}
+
+.slide1 {
+   background - image: url("../image/photo-1483068612337-c045daaca40e.jpg");
+}
+
+.slide2 {
+   background - image: url("../image/photo-1489914099268-1dad649f76bf.jpg");
+}
+
+.slide3 {
+   background - image: url("../image/photo-1490100667990-4fced8021649.jpg");
+}
+
+.slide4 {
+   background - image: url("../image/photo-1494783404829-a93883e74b68.jpg");
+}
+
+.slider {
+   position: absolute;
+   width: 400 % ;
+   height: 100 % ;
+   background: #000;
+
+	display: inline-flex;
+
+	overflow: hidden;
+
+}
+
+
+
+.slide1,
+
+.slide2,
+
+.slide3,
+
+.slide4 {
+
+	position: absolute;
+
+	background-position: center;
+
+	background-size: cover;
+
+	color: # fff;
+   font - size: 62 px;
+   padding - top: 138 px;
+   font - weight: 800;
+   font - family: "Heebo",
+   sans - serif;
+   text - align: center;
+   width: 25 % ;
+   height: 100 % ;
+   z - index: 10;
+   transition: 1.4 s;
+}
+
+.tran {
+   transform: scale(1.3);
+}
+
+.up1 {
+   z - index: 20;
+}
+
+.up2 {
+   z - index: 40;
+}
+
+.steap {
+   stroke - width: 0;
+}
+
+.streak {
+   stroke - width: 82 px;
+}
+
+@media(max - width: 700 px) {
+   .parent {
+      margin - left: 1 % ;
+   }
+}@import url("2-css.css");
+
+*{
+   margin: 0;
+   padding: 0;
+   box - sizing: border - box;
+}
+
+.parent {
+   width: 681 px;
+   height: 384 px;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   margin: auto auto;
+   overflow: hidden;
+   position: absolute;
+   border - radius: 16 px; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+svg {
+   position: absolute;
+   z - index: 1;
+   width: 681 px;
+   height: 384 px;
+}
+
+button {
+   position: absolute;
+   z - index: 50;
+   width: 40 px;
+   overflow: hidden;
+   height: 40 px;
+   border: none;
+   border - radius: 50 % ;
+   background: #fff;
+   cursor: pointer; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+button: focus {
+   outline - width: 0;
+}
+
+circle {
+   stroke: #fff;
+   fill: none;
+   transition: 0.3 s;
+}
+
+#svg1 circle {
+   transition - timing - function: linear;
+}
+
+#svg2 circle {
+   transition - timing - function: linear;
+}
+
+#Capa_1 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-7 px, -8 px);
+}
+
+#Capa_2 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-9 px, -8 px);
+}
+
+.right {
+   margin - left: 628 px;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.right:hover {
+
+	background-color: # fff;
+}
+
+.left {
+   margin - left: 0.5 % ;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.left:hover {
+
+	background-color: # fff;
+}
+
+.circle1 {
+   transition - delay: 0.05 s;
+}
+
+.circle2 {
+   transition - delay: 0.1 s;
+}
+
+.circle3 {
+   transition - delay: 0.15 s;
+}
+
+.circle4 {
+   transition - delay: 0.2 s;
+}
+
+.circle5 {
+   transition - delay: 0.25 s;
+}
+
+.circle6 {
+   transition - delay: 0.3 s;
+}
+
+.circle7 {
+   transition - delay: 0.35 s;
+}
+
+.circle8 {
+   transition - delay: 0.4 s;
+}
+
+.circle9 {
+   transition - delay: 0.45 s;
+}
+
+.circle10 {
+   transition - delay: 0.05 s;
+}
+
+.circle11 {
+   transition - delay: 0.1 s;
+}
+
+.circle12 {
+   transition - delay: 0.15 s;
+}
+
+.circle13 {
+   transition - delay: 0.2 s;
+}
+
+.circle14 {
+   transition - delay: 0.25 s;
+}
+
+.circle15 {
+   transition - delay: 0.3 s;
+}
+
+.circle16 {
+   transition - delay: 0.35 s;
+}
+
+.circle17 {
+   transition - delay: 0.4 s;
+}
+
+.circle18 {
+   transition - delay: 0.45 s;
+}
+
+.slide1 {
+   background - image: url("../image/photo-1483068612337-c045daaca40e.jpg");
+}
+
+.slide2 {
+   background - image: url("../image/photo-1489914099268-1dad649f76bf.jpg");
+}
+
+.slide3 {
+   background - image: url("../image/photo-1490100667990-4fced8021649.jpg");
+}
+
+.slide4 {
+   background - image: url("../image/photo-1494783404829-a93883e74b68.jpg");
+}
+
+.slider {
+   position: absolute;
+   width: 400 % ;
+   height: 100 % ;
+   background: #000;
+
+	display: inline-flex;
+
+	overflow: hidden;
+
+}
+
+
+
+.slide1,
+
+.slide2,
+
+.slide3,
+
+.slide4 {
+
+	position: absolute;
+
+	background-position: center;
+
+	background-size: cover;
+
+	color: # fff;
+   font - size: 62 px;
+   padding - top: 138 px;
+   font - weight: 800;
+   font - family: "Heebo",
+   sans - serif;
+   text - align: center;
+   width: 25 % ;
+   height: 100 % ;
+   z - index: 10;
+   transition: 1.4 s;
+}
+
+.tran {
+   transform: scale(1.3);
+}
+
+.up1 {
+   z - index: 20;
+}
+
+.up2 {
+   z - index: 40;
+}
+
+.steap {
+   stroke - width: 0;
+}
+
+.streak {
+   stroke - width: 82 px;
+}
+
+@media(max - width: 700 px) {
+   .parent {
+      margin - left: 1 % ;
+   }
+}@import url("2-css.css");
+
+*{
+   margin: 0;
+   padding: 0;
+   box - sizing: border - box;
+}
+
+.parent {
+   width: 681 px;
+   height: 384 px;
+   top: 0;
+   bottom: 0;
+   left: 0;
+   right: 0;
+   margin: auto auto;
+   overflow: hidden;
+   position: absolute;
+   border - radius: 16 px; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+svg {
+   position: absolute;
+   z - index: 1;
+   width: 681 px;
+   height: 384 px;
+}
+
+button {
+   position: absolute;
+   z - index: 50;
+   width: 40 px;
+   overflow: hidden;
+   height: 40 px;
+   border: none;
+   border - radius: 50 % ;
+   background: #fff;
+   cursor: pointer; -
+   webkit - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75); -
+   moz - box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+   box - shadow: 0 0 88 px 5 px rgba(0, 0, 0, 0.75);
+}
+
+button: focus {
+   outline - width: 0;
+}
+
+circle {
+   stroke: #fff;
+   fill: none;
+   transition: 0.3 s;
+}
+
+#svg1 circle {
+   transition - timing - function: linear;
+}
+
+#svg2 circle {
+   transition - timing - function: linear;
+}
+
+#Capa_1 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-7 px, -8 px);
+}
+
+#Capa_2 {
+   position: absolute;
+   width: 16 px;
+   height: 16 px;
+   transform: translate(-9 px, -8 px);
+}
+
+.right {
+   margin - left: 628 px;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.right:hover {
+
+	background-color: # fff;
+}
+
+.left {
+   margin - left: 0.5 % ;
+   margin - top: 168 px;
+   border: 1 px solid #849494;
+
+	background-color: transparent;
+
+	transition: 0.5s;
+
+}
+
+
+
+.left:hover {
+
+	background-color: # fff;
+}
+
+.circle1 {
+   transition - delay: 0.05 s;
+}
+
+.circle2 {
+   transition - delay: 0.1 s;
+}
+
+.circle3 {
+   transition - delay: 0.15 s;
+}
+
+.circle4 {
+   transition - delay: 0.2 s;
+}
+
+.circle5 {
+   transition - delay: 0.25 s;
+}
+
+.circle6 {
+   transition - delay: 0.3 s;
+}
+
+.circle7 {
+   transition - delay: 0.35 s;
+}
+
+.circle8 {
+   transition - delay: 0.4 s;
+}
+
+.circle9 {
+   transition - delay: 0.45 s;
+}
+
+.circle10 {
+   transition - delay: 0.05 s;
+}
+
+.circle11 {
+   transition - delay: 0.1 s;
+}
+
+.circle12 {
+   transition - delay: 0.15 s;
+}
+
+.circle13 {
+   transition - delay: 0.2 s;
+}
+
+.circle14 {
+   transition - delay: 0.25 s;
+}
+
+.circle15 {
+   transition - delay: 0.3 s;
+}
+
+.circle16 {
+   transition - delay: 0.35 s;
+}
+
+.circle17 {
+   transition - delay: 0.4 s;
+}
+
+.circle18 {
+   transition - delay: 0.45 s;
+}
+
+.slide1 {
+   background - image: url("../image/photo-1483068612337-c045daaca40e.jpg");
+}
+
+.slide2 {
+   background - image: url("../image/photo-1489914099268-1dad649f76bf.jpg");
+}
+
+.slide3 {
+   background - image: url("../image/photo-1490100667990-4fced8021649.jpg");
+}
+
+.slide4 {
+   background - image: url("../image/photo-1494783404829-a93883e74b68.jpg");
+}
+
+.slider {
+   position: absolute;
+   width: 400 % ;
+   height: 100 % ;
+   background: #000;
+
+	display: inline-flex;
+
+	overflow: hidden;
+
+}
+
+
+
+.slide1,
+
+.slide2,
+
+.slide3,
+
+.slide4 {
+
+	position: absolute;
+
+	background-position: center;
+
+	background-size: cover;
+
+	color: # fff;
+   font - size: 62 px;
+   padding - top: 138 px;
+   font - weight: 800;
+   font - family: "Heebo",
+   sans - serif;
+   text - align: center;
+   width: 25 % ;
+   height: 100 % ;
+   z - index: 10;
+   transition: 1.4 s;
+}
+
+.tran {
+   transform: scale(1.3);
+}
+
+.up1 {
+   z - index: 20;
+}
+
+.up2 {
+   z - index: 40;
+}
+
+.steap {
+   stroke - width: 0;
+}
+
+.streak {
+   stroke - width: 82 px;
+}
+
+@media(max - width: 700 px) {
+   .parent {
+      margin - left: 1 % ;
+   }
+}// For the thumbnail demo! :]
+
+var count = 1
+setTimeout(demo, 500)
+setTimeout(demo, 700)
+setTimeout(demo, 900)
+setTimeout(reset, 2000)
+
+setTimeout(demo, 2500)
+setTimeout(demo, 2750)
+setTimeout(demo, 3050)
+
+
+var mousein = false
+function demo() {
+   if(mousein) return
+   document.getElementById('demo' + count++)
+      .classList.toggle('hover')
+   
+}
+
+function demo2() {
+   if(mousein) return
+   document.getElementById('demo2')
+      .classList.toggle('hover')
+}
+
+function reset() {
+   count = 1
+   var hovers = document.querySelectorAll('.hover')
+   for(var i = 0; i < hovers.length; i++ ) {
+      hovers[i].classList.remove('hover')
+   }
+}
+
+document.addEventListener('mouseover', function() {
+   mousein = true
+   reset()
+})
